9fe60c35663a4c3a4b9168de0b4fdae860f82ae6
Ryan <ryankanno@localkinegrinds.com>
2021-12-30 19:41:09 +0900

    Added hangouts/jitsi to open in Chrome, zoom links to open in Zoom

--- dot_finicky.js
+++ dot_finicky.js
@@ -20,6 +20,18 @@ module.exports = {
     {
       match: ({ url }) => url.protocol === "tweetbot",
       browser: "com.tapbots.Tweetbot3Mac"
+    },
+    {
+      match: finicky.matchHostnames(["meet.jit.si", "meet.google.com", "hangouts.google.com"]),
+      browser: "Google Chrome"
+    },
+    {
+      match: [
+        "zoom.us/*",
+        finicky.matchDomains(/.*\zoom.us/),
+        /zoom.us\/j\//,
+      ],
+      browser: "us.zoom.xos"
     }
   ]
 };
