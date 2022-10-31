3046ccf7fe42f0186f4d615aa939ad0d56847e4c
Ryan <ryankanno@localkinegrinds.com>
2022-10-31 14:19:16 +0900

    feat: added fzf commands for linux-gnu

--- dot_fzf.bash
+++ dot_fzf.bash
@@ -1,11 +1,20 @@
 # enable fzf keybindings
-source /opt/local/share/fzf/shell/key-bindings.bash
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  source /opt/local/share/fzf/shell/key-bindings.bash
+
+  # enable fzf fuzzy completion
+  [[ $- == *i* ]] && source /opt/local/share/fzf/shell/completion.bash 2> /dev/null
+fi
+
+if [[ "$OSTYPE" == "linux-gnu" ]]; then
+  source /usr/share/doc/fzf/examples/key-bindings.bash
+
+  # enable fzf fuzzy completion
+  [[ $- == *i* ]] && source /usr/share/doc/fzf/examples/completion.bash 2> /dev/null
+fi
 
 # w/ modifications
 export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules,.*cache,__*cache__}/*" 2> /dev/null'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="bfs -type d -nohidden"
 bind -x '"\C-p": nvim $(fzf);'
-
-# enable fzf fuzzy completion
-[[ $- == *i* ]] && source /opt/local/share/fzf/shell/completion.bash 2> /dev/null
