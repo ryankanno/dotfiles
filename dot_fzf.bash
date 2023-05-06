61545148c2cdbc4ecb723a3dc1c660049357371d
Ryan <ryankanno@localkinegrinds.com>
2023-05-06 10:03:36 +0900

    fix: added .tox to fzf_default_command

--- dot_fzf.bash
+++ dot_fzf.bash
@@ -14,7 +14,7 @@ if [[ "$OSTYPE" == "linux-gnu" ]]; then
 fi
 
 # w/ modifications
-export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules,.*cache,__*cache__}/*" 2> /dev/null'
+export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,.tox,node_modules,.*cache,__*cache__}/*" 2> /dev/null'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 export FZF_ALT_C_COMMAND="bfs -type d -nohidden"
 bind -x '"\C-p": nvim $(fzf);'
