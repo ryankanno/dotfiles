28b1e2fc1f09ee2aa2fe4fb82192f97fe344b298
Ryan <ryankanno@localkinegrinds.com>
2023-05-06 09:58:10 +0900

    fix: removed tapbots from finicky

--- dot_finicky.js
+++ dot_finicky.js
@@ -1,26 +1,8 @@
 // ~/.finicky.js
 
-const regex = /^https?:\/\/twitter\.com\/(?:#!\/)?(\w+)\/status(?:es)?\/(\d+).*$/
-
 module.exports = {
   defaultBrowser: "Firefox",
-  rewrite: [
-    {
-      match: ({urlString, opener}) => {
-        const matches = urlString.match(regex);
-        return matches && matches[2] && opener.bundleId !== "com.tapbots.Tweetbot3Mac";
-      },
-      url: ({ urlString }) => {
-        const [_, user, statusId] = urlString.match(regex);
-        return `tweetbot://${user}/status/${statusId}`;
-      }
-    }
-  ],
   handlers: [
-    {
-      match: ({ url }) => url.protocol === "tweetbot",
-      browser: "com.tapbots.Tweetbot3Mac"
-    },
     {
       match: "open.spotify.com/*",
       browser: "Spotify"
