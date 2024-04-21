4aabbdb4400431b043ae9614a03fce3b42043b1a
Ryan <ryankanno@localkinegrinds.com>
2024-04-21 13:51:36 -1000

    chore: removed ncdu + added comments

--- dot_config/home-manager/home.nix
+++ dot_config/home-manager/home.nix
@@ -3,58 +3,59 @@
 }: let
 
 unstable-packages = with unstable; [
-    atuin
-    bandwhich
-    bat
-    btop
+    atuin          # shell history (https://github.com/atuinsh/atuin)
+    bandwhich      # terminal bandwidth (https://github.com/imsnif/bandwhich)
+    bat            # cat alternative (https://github.com/sharkdp/bat)
+    btop           # resource monitor (https://github.com/aristocratos/btop)
     coreutils
     curl
-    delta
-    difftastic
-    diskonaut
-    duc
-    du-dust
-    erdtree
-    eza
-    fd
+    delta          # pager (https://github.com/dandavison/delta)
+    difftastic     # structural diff (https://github.com/Wilfred/difftastic)
+    diskonaut      # disk usage (https://github.com/imsnif/diskonaut)
+    dua            # disk usage (https://github.com/Byron/dua-cli)
+    duc            # disk usage (https://duc.zevv.nl/)
+    du-dust        # disk usage (https://github.com/bootandy/dust)
+    erdtree        # fs + disk usage (https://github.com/solidiquis/erdtree)
+    eza            # ls alternative (https://github.com/eza-community/eza)
+    fd             # find alternative (https://github.com/sharkdp/fd)
     findutils
-    fq
-    fx
+    fq             # jq for binary (https://github.com/wader/fq)
+    fx             # terminal json (https://github.com/antonmedv/fx)
     git
     git-crypt
-    glow
-    hyperfine
-    jq
+    glow           # terminal markdown (https://github.com/charmbracelet/glow)
+    hyperfine      # benchmarking (https://github.com/sharkdp/hyperfine)
+    jq             # json processor (https://github.com/jqlang/jq)
     killall
-    miller
-    mosh
-    ncdu
-    neofetch
+    miller         # https://github.com/johnkerl/miller
+    mosh           # mobile shell (https://github.com/mobile-shell/mosh)
+    neofetch       # system info tool (https://github.com/dylanaraps/neofetch)
     neovim
-    onefetch
+    onefetch       # git info tool (https://github.com/o2sh/onefetch)
     parallel
-    prettyping
-    procs
-    progress
-    restic
+    prettyping     # ping wrapper (https://github.com/denilsonsa/prettyping)
+    procs          # ps alternative (https://github.com/dalance/procs)
+    progress       # show progress (https://github.com/Xfennec/progress)
+    pv
+    restic         # backup (https://github.com/restic/restic)
     ripgrep
-    sd
-    taplo
-    tealdeer
+    sd             # sed alternative (https://github.com/chmln/sd)
+    taplo          # toml (https://github.com/tamasfe/taplo)
+    tealdeer       # tldr (https://github.com/dbrgn/tealdeer)
     tmux
-    tokei
-    trash-cli
+    tokei          # cloc alternative (https://github.com/XAMPPRocky/tokei)
+    trash-cli      # trash cli (https://github.com/andreafrancia/trash-cli)
     tree
-    tree-sitter
+    tree-sitter    # parser generator (https://github.com/tree-sitter/tree-sitter)
     unzip
     vim
     wget
     zip
-    zoxide
+    zoxide         # smarter cd (https://github.com/ajeetdsouza/zoxide)
 
     # funsies
-    figlet
-    toilet
+    figlet         # https://github.com/cmatsuoka/figlet
+    toilet         # https://github.com/cacalabs/toilet
 ];
 
 stable-packages = with pkgs; [
