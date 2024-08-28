eaa20a591ac1222c68a078753dc13bb7f14c701e
Ryan <ryankanno@localkinegrinds.com>
2024-08-28 10:55:00 -1000

    chore: added darwin config

---
+++ dot_config/home-manager/darwin.nix
@@ -0,0 +1,92 @@
+{
+    config, pkgs, unstable, ...
+}: let
+
+unstable-tools-packages = with unstable; [
+    age            # encryption tool (https://github.com/FiloSottile/age)
+    atuin          # shell history (https://github.com/atuinsh/atuin)
+    awscli2
+    bandwhich      # terminal bandwidth (https://github.com/imsnif/bandwhich)
+    bat            # cat alternative (https://github.com/sharkdp/bat)
+    btop           # resource monitor (https://github.com/aristocratos/btop)
+    clipboard-jh   # clipboard (https://github.com/Slackadays/Clipboard)
+    colima         # container runtime (https://github.com/abiosoft/colima)
+    coreutils
+    curl
+    delta          # pager (https://github.com/dandavison/delta)
+    difftastic     # structural diff (https://github.com/Wilfred/difftastic)
+    diskonaut      # disk usage (https://github.com/imsnif/diskonaut)
+    dua            # disk usage (https://github.com/Byron/dua-cli)
+    du-dust        # disk usage (https://github.com/bootandy/dust)
+    erdtree        # fs + disk usage (https://github.com/solidiquis/erdtree)
+    eza            # ls alternative (https://github.com/eza-community/eza)
+    fd             # find alternative (https://github.com/sharkdp/fd)
+    findutils
+    fq             # jq for binary (https://github.com/wader/fq)
+    fx             # terminal json (https://github.com/antonmedv/fx)
+    git
+    git-crypt
+    git-open       # https://github.com/paulirish/git-open
+    glow           # terminal markdown (https://github.com/charmbracelet/glow)
+    hyperfine      # benchmarking (https://github.com/sharkdp/hyperfine)
+    jq             # json processor (https://github.com/jqlang/jq)
+    just           # command runner (https://github.com/casey/just)
+    killall
+    litestream     # https://github.com/benbjohnson/litestream
+    miller         # https://github.com/johnkerl/miller
+    moreutils      # https://joeyh.name/code/moreutils/
+    mosh           # mobile shell (https://github.com/mobile-shell/mosh)
+    neofetch       # system info tool (https://github.com/dylanaraps/neofetch)
+    onefetch       # git info tool (https://github.com/o2sh/onefetch)
+    (lib.hiPrio parallel)
+    prettyping     # ping wrapper (https://github.com/denilsonsa/prettyping)
+    procs          # ps alternative (https://github.com/dalance/procs)
+    progress       # show progress (https://github.com/Xfennec/progress)
+    pv
+    restic         # backup (https://github.com/restic/restic)
+    ripgrep
+    ripgrep-all    # https://github.com/phiresky/ripgrep-all
+    sd             # sed alternative (https://github.com/chmln/sd)
+    shellcheck
+    taplo          # toml (https://github.com/tamasfe/taplo)
+    tealdeer       # tldr (https://github.com/dbrgn/tealdeer)
+    tmux
+    toastify       # command line tool using notify-rust (https://github.com/hoodie/toastify)
+    tokei          # cloc alternative (https://github.com/XAMPPRocky/tokei)
+    trash-cli      # trash cli (https://github.com/andreafrancia/trash-cli)
+    tree
+    tree-sitter    # parser generator (https://github.com/tree-sitter/tree-sitter)
+    unzip
+    upterm         # terminal sharing (https://github.com/owenthereal/upterm)
+    vim
+    watchexec      # runs commands on modification (https://github.com/watchexec/watchexec)
+    wget
+    zip
+    zoxide         # smarter cd (https://github.com/ajeetdsouza/zoxide)
+
+    # funsies
+    figlet         # https://github.com/cmatsuoka/figlet
+    toilet         # https://github.com/cacalabs/toilet
+];
+
+unstable-language-packages = with unstable; [
+    cargo
+    rustc
+];
+
+stable-packages = with pkgs; [
+    gh
+    cargo-cache
+    cargo-expand
+];
+
+in
+{
+    home.username = "ryankanno";
+    home.homeDirectory = "/Users/ryankanno";
+    home.stateVersion = "23.11"; # Please read the comment before changing.
+    home.packages = stable-packages ++ unstable-tools-packages ++ unstable-language-packages ++ [];
+    home.file = {};
+    home.sessionVariables = {};
+    programs.home-manager.enable = true;
+}
--- dot_config/home-manager/flake.nix
+++ dot_config/home-manager/flake.nix
@@ -2,21 +2,35 @@
   description = "NixOS configuration";
 
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
+  inputs.nixpkgs-darwin.url = "github:nixos/nixpkgs/nixpkgs-24.05-darwin";
   inputs.nixpkgs-unstable.url = "github:nixos/nixpkgs/nixos-unstable";
+
+  inputs.nix-darwin.url = "github:LnL7/nix-darwin";
+  inputs.nix-darwin.inputs.nixpkgs.follows = "nixpkgs-darwin";
 
   inputs.home-manager.url = "github:nix-community/home-manager/release-24.05";
   inputs.home-manager.inputs.nixpkgs.follows = "nixpkgs";
 
-  outputs = inputs @ { nixpkgs, nixpkgs-unstable, home-manager, ... }:
+  outputs = inputs @ { nixpkgs, nixpkgs-darwin, nixpkgs-unstable, nix-darwin, home-manager, ... }:
     let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-      unstable = import nixpkgs-unstable { inherit system; };
+      linux_system = "x86_64-linux";
+      linux_pkgs = nixpkgs.legacyPackages.${linux_system};
+      linux_unstable = import nixpkgs-unstable { system = linux_system; };
+      darwin_system = "aarch64-darwin";
+      darwin_pkgs = nixpkgs.legacyPackages.${darwin_system};
+      darwin_unstable = import nixpkgs-unstable { system = darwin_system; };
     in {
-      homeConfigurations."ryankanno" = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [ ./home.nix ];
-        extraSpecialArgs = { inherit unstable; };
+      # home-manager switch --flake .#ryankanno@linux
+      homeConfigurations."ryankanno@linux" = home-manager.lib.homeManagerConfiguration {
+        pkgs = linux_pkgs;
+        modules = [ ./linux.nix ];
+        extraSpecialArgs = { unstable = linux_unstable; };
+      };
+      # home-manager switch --flake .#ryankanno@macmini
+      homeConfigurations."ryankanno@macmini" = home-manager.lib.homeManagerConfiguration {
+        pkgs = darwin_pkgs;
+        modules = [ ./darwin.nix ];
+        extraSpecialArgs = { unstable = darwin_unstable; };
       };
     };
 }
